aa47ff597beb2d5418fda44151fe5ae0af8d1873
Toan <tpkhuong@gmail.com>
2020-11-28 15:13:52 -0500

    add webpack.config file and ran built

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -7,3 +7,15 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
 };
+
+/*
+If a webpack.config.js is present, the webpack command picks it up by default. We use the --config option here only to show that you can pass a configuration of any name.
+This will be useful for more complex configurations that need to be split into multiple files.
+*/
+
+/*
+Out of the box, webpack won't require you to use a configuration file. However, it will assume the entry point of your project is src/index.js
+and will output the result in dist/main.js minified and optimized for production.
+
+Usually your projects will need to extend this functionality, for this you can create a webpack.config.js file in the root folder and webpack will automatically use it.
+*/
