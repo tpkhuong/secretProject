7b13b5818fcd3eb90adee709819f091f61bd1f18
Toan <tpkhuong@gmail.com>
2020-11-28 16:53:33 -0500

    final commit for the day. continue learning tomorrow

--- webpack-demo/app.js
+++ webpack-demo/app.js
@@ -6,13 +6,17 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+/*
+__dirname only cares about where this file is or what directory this file is located.
+*/
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", function rootPage(req, res) {
-  res.render("index");
+  var title = "I love Everything about programming. Let's go!";
+  res.render("index", { title });
 });
 
 app.listen(PORT, function listenOn() {
