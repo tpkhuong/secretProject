78596d1f35000b868529e6347615b3113bd7debc
Toan <tpkhuong@gmail.com>
2020-11-30 07:56:22 -0500

    first commit of the day. continue our webpack learning journey

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -3,9 +3,12 @@ var path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  // module: {
+  //   rules:
+  // }
 };
 
 /*
