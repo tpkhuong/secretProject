80769b52a90ad3aada3467ee8953cf31af18762f
Toan <tpkhuong@gmail.com>
2020-12-01 10:04:33 -0500

    first commit of the day

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -8,7 +8,7 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  
+
   btn.innerHTML = "Click me and check the console!";
   btn.onclick = printMe;
 
